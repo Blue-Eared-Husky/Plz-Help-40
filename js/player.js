ea57de7d6814636180f4ca65558d482d34e2d93c
Blue-Eared-Husky <81889254+Blue-Eared-Husky@users.noreply.github.com>
2021-08-04 03:16:40 -0700

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,55 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+        this.rank = 0;
+    }
+
+    
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    getPlayerAtEnd(){
+        var playerAtEndRef = database.ref('playerAtEnd');
+        playerAtEndRef.on("value", (data) => {
+            playerAtEnd = data.val();
+        })
+    }
+
+    static updatePlayerAtEnd(count){
+        database.ref('/').update({
+            playerAtEnd: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
